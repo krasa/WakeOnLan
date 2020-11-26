3f04017216f11d2f8d13890b357dc6f0f6df9271
Vojtěch Krása <vojta.krasa@gmail.com>
2020-11-26 12:35:37 +0100

    start

--- src/main/java/krasa/wakeonlan/Launcher.java
+++ src/main/java/krasa/wakeonlan/Launcher.java
@@ -4,9 +4,9 @@ import javafx.application.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class WakeOnLanApplication {
+public class Launcher {
 
-	private static final Logger log = LoggerFactory.getLogger(WakeOnLanApplication.class);
+	private static final Logger log = LoggerFactory.getLogger(Launcher.class);
 
 	public static void main(String[] args) {
 		Application.launch(JavaFxApplication.class, args);
